a1fd18980de8ffc9bea59e9dc596fe2768e82e14
nicollylemos <nicollylemos752@gmail.com>
2024-06-30 17:07:23 -0300

    Tela Inicial (1.1)

--- inc/header.php
+++ inc/header.php
@@ -6,13 +6,13 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
-    <link rel="stylesheet" href="css/css/header.css">
+    <link rel="stylesheet" href="css/css/cabecalho.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
   </head>
   <body class="p-0 m-0 border-0 bd-example m-0 border-0">
-    <nav class="navbar navbar-expand-lg">
+    <nav id="navbar" class="navbar navbar-expand-lg">
       <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">VacinePet</a>
+        <a class="navbar-brand" href="index.php">VacinePet </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
--- index.php
+++ index.php
@@ -6,15 +6,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/css/style.css">
+    <link rel="stylesheet" href="css/css/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.css">
     <title>VacinePet</title>
 </head>
 <body>
    <section class="home-inicial">
     <div class="tela-inicial">
         <div class="info-inicial">
-            <h1>VacinePet</h1>
-            <h2>CARINHO, CUIDADO E PREVENÇÃO PARA <br>SEU PET, NO CONFORTO DA SUA CASA.</h2>
+            <h1 class="titulo-site">VacinePet <i class="fa-solid fa-paw fa-rotate-by" style="color: #fa5909; --fa-rotate-angle: 49deg; width: 30px; height:10px; font-size:40px;"></i></h1>
+            <h2 class="subtitulo-site">CARINHO, CUIDADO E PREVENÇÃO PARA <br>SEU PET, NO CONFORTO DA SUA CASA.</h2>
             <p> Lorem Ipsum is simply dummy text of the printing and <br> typesetting industry. Lorem Ipsum has been the industry's<br> standard dummy text ever since the 1500s, when an <br>unknown printer took a galley of type and scrambled <br>it to make a type specimen book. </p>
             <button>Agendar</button>
         </div>
