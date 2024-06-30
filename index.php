1223c4c14e8aaebc0617540e3288d935ce8ec3c0
nicollylemos <nicollylemos752@gmail.com>
2024-06-30 15:49:12 -0300

    index add (nico)

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php 
+ include("./inc/header.php")
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/css/style.css">
+    <title>VacinePet</title>
+</head>
+<body>
+   <section class="home-inicial">
+    <div class="tela-inicial">
+        <div class="info-inicial">
+            <h1>VacinePet</h1>
+            <h2>CARINHO, CUIDADO E PREVENÇÃO PARA <br>SEU PET, NO CONFORTO DA SUA CASA.</h2>
+            <p> Lorem Ipsum is simply dummy text of the printing and <br> typesetting industry. Lorem Ipsum has been the industry's<br> standard dummy text ever since the 1500s, when an <br>unknown printer took a galley of type and scrambled <br>it to make a type specimen book. </p>
+            <button>Agendar</button>
+        </div>
+        <div class="img-animal">
+            <img src="" alt="">
+        </div>
+    </div>
+   </section>
+   <section class="info-doencas">
+    <div class="informacao">
+        <div class="informacao-cao">
+            <div class="texto-info">
+                <h3>CÃO</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting <br> industry. Lorem Ipsum has been the industry's standard dummy <br>text ever since the 1500s, when and.
+            </div>
+        </div>
+       <div class="informacao-gato">
+            <div class="texto-info">
+                <h3>GATO</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting <br> industry. Lorem Ipsum has been the industry's standard dummy <br>text ever since the 1500s, when and.
+            </div>
+       </div>
+    </div>
+   </section>
+   <section class="atendimento-dmcl">
+
+   </section>
+</body>
+</html>
