6695eb49a6bee6dd215204a964a5414365735da3
rafamans4no <rafaela.fernandes48@etec.sp.gov.br>
2024-07-03 20:43:44 -0300

    oioi

--- login.php
+++ login.php
@@ -9,51 +9,48 @@
     <title>Login</title>
 </head>
 
-<body>
+    <body>
 
-    <div class="container">
-        <div class="yellow-div">
-            <div class="cilindros cilindro1"></div>
-            <div class="cilindros cilindro2"></div>
-            <div class="cilindros cilindro3"></div>
-            <div class="cilindros cilindro4"></div>
-            <div class="cilindros cilindro5"></div>
-        </div>
+        <div class="container">
+            <div class="yellow-div">
+                <div class="cilindros cilindro1"></div>
+                <div class="cilindros cilindro2"></div>
+                <div class="cilindros cilindro3"></div>
+                <div class="cilindros cilindro4"></div>
+                <div class="cilindros cilindro5"></div>
+            </div>
 
-        <div class="quad">
-            <div class="form">
-                <form action="login.php" method="post">
+            <div class="quad">
+                <div class="form">
+                    <form action="login.php" method="post">
 
-                    <div class="title">
-                        <h1 class="title-login"> LOGINNNNN <i style="color: #fa5909; --fa-rotate-angle: 49deg; "></i></h1>
-                    </div>
+                        <div class="title">
+                            <h1 class="title-login"> LOGIN <i style="color: #fa5909; --fa-rotate-angle: 49deg;"></i></h1>
+                        </div>
 
-                    <div class="form-group mt-4">
-                        <input type="text" maxlength="255" placeholder="Email" class="form-control"
-                            name="customer['name']">
-                    </div>
+                        <div class="form-group mt-4">
+                            <input type="text" maxlength="255" placeholder="Email" class="form-control" name="customer['name']">
+                        </div>
 
-                    <div class="form-group mt-4">
-                        <input type="password" maxlength="11" placeholder="Senha" class="form-control"
-                            name="customer['cpf_cnpj']">
-                    </div>
+                        <div class="form-group mt-4">
+                            <input type="password" maxlength="11" placeholder="Senha" class="form-control" name="customer['cpf_cnpj']">
+                        </div>
 
-                    <a href="#">Esqueceu sua senha?</a>
+                        <a href="#" class="esquecer">Esqueceu sua senha?</a>
 
-                    <div id="actions" class="mt-4">
-                        <button type="submit" class="btn btn-primary"
-                            style="background-color: #FD811C; border-color: #FD811C;">
-                            <i class="fa-solid fa-angle-right"></i> ENTRAR  
-                        </button>
-                    </div>
+                        <div class="final">
+                            <div id="actions" class="mt-4">
+                                <button type="submit" class="btn btn-primary btn-custom">ENTRAR</button>
+                            </div>
 
-                    <div class="fim">
-                        <h7>Não tem uma conta?</h7> <a href="#">Cadastre-se</a>
-                    </div>
-                </form>
+                            <div class="criar-div">
+                                <h7>Não tem uma conta?</h7><a href="#"class="criar">Cadastre-se</a>
+                            </div>
+                        </div>
+                        
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-
-</body>
+    </body>
 </html>
