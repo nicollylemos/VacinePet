a3033f5e6260f733a413c0d0372badcbd1d66549
nicollylemos <nicollylemos752@gmail.com>
2024-07-10 17:52:14 -0300

    testando

--- inc/footer.php
+++ inc/footer.php
@@ -4,23 +4,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/css/footer.css">
+    <link rel="stylesheet" href="css/css/rodape.css">
 </head>
 
 <body>
-    <h1>VacinePet <i class="fa-solid fa-paw fa-rotate-by"
-            style="color: #fa5909; --fa-rotate-angle: 49deg; width: 13px; height:7px; font-size:15px;"></i></h1>
-    <div class="infos-site">
-        <ul>
-            <li>Funcionamento de Segunda a Sexta</li>
-            <li>Telefone: (XX) XXXXX-XXXX</li>
-            <li>Email: vacinpet@gmail.com</li>
-        </ul>
-        <ul>
-            <li><a href="#">Política de Privacidade</a></li>
-            <li><a href="#">Termo de Uso</a></li>
-            <li><a href="#">Direitos Autorais</a></li>
-        </ul>
+    <div class="footer">
+        <h1>VacinePet <i class="fa-solid fa-paw fa-rotate-by"
+                style="color: #fff; --fa-rotate-angle: 49deg; width: 13px; height:7px; font-size:15px;"></i></h1>
+        <div class="infos-site">
+            <ul>
+                <li>
+                    <h2>Contato</h2>
+                </li>
+                <li>Funcionamento de Segunda a Sexta</li>
+                <li>Telefone: (XX) XXXXX-XXXX</li>
+                <li>Email: vacinepet@gmail.com</li>
+            </ul>
+            <ul>
+                <li>
+                    <h2>Políticas</h2>
+                </li>
+                <li><a href="#">Política de Privacidade</a></li>
+                <li><a href="#">Termo de Uso</a></li>
+                <li><a href="#">Direitos Autorais</a></li>
+            </ul>
+            <ul>
+                <li>
+                    <h2>Sobre nós</h2>
+                </li>
+                <li><a href="#">Sobre o VacinePet</a></li>
+                <li><a href="#">Sobre o profissional</a></li>
+                <li><a href="#">Sobre o atendimento</a></li>
+            </ul>
+
+        </div>
     </div>
 </body>
 
