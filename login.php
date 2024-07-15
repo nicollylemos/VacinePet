ca20c2770ee1134a1881650ccc182971d49b8e70
rafamans4no <rafaela.fernandes48@etec.sp.gov.br>
2024-07-15 12:43:53 -0300

    1507

--- login.php
+++ login.php
@@ -3,10 +3,10 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=10, minimum-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
     <link rel="stylesheet" href="css/css/entrar.css">
-    <link rel="stylesheet" href="css/css/responsividade/login.css">
     <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=10, minimum-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.css">
     <title>Login</title>
