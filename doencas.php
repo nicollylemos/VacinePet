ec59149358c6f7dd31ef8c80cdda8694b3a8607c
rafamans4no <rafaela.fernandes48@etec.sp.gov.br>
2024-07-17 18:02:26 -0300

    1707

---
+++ doencas.php
@@ -0,0 +1,38 @@
+<?php
+include ("./inc/header.php")
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/css/doencas.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.css">
+    <title>Doenças Caninas</title>
+</head>
+<body>
+    <section class="start">
+        <div class="espaco"></div>
+        <div class="escritos">
+            <h1 class="title-start">DOENÇAS<br>CANINAS</h1>
+            <h4 class="sub-title-start">Conheça as doenças caninas e proteja seu melhor amigo!<br>
+                Saber mais sobre infecções e problemas genéticos<br>
+                é essencial para a saúde do seu cão. Identifique<br>
+                sintomas cedo e assegure um tratamento eficaz.<br>
+                Previna doenças com vacinação e cuidados regulares.<br>
+                Informe-se e fortaleça o vínculo com seu pet.<br>
+                Gostaria de conhecer mais sobre as doenças caninas?</h4>
+            <button onclick="scrollToSection()" type="submit" class="btn-start">CONHECER</button>
+            <div class="splash-container">
+                <img src="css/css/img/beagle.png" class="img-dog" alt="Dog">
+            </div>
+        </div>
+    </section>
+    <section id="doencas">
+        carrossel
+    </section>
+</body>
+<?php include ("./inc/footer.php") ?>
+<!--scroll JS-->
+<script src="js/scroll.js"></script>
+</html>
