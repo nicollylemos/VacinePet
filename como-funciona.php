4aebc4747e424b9cb72e27950fe9c45afd9019aa
EduardaOS <dudaesouza26@gmail.com>
2024-07-22 20:47:44 -0300

    "como-funcions" finalizado

--- como-funciona.php
+++ como-funciona.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
-    <link rel="stylesheet" href="css/css/comofunciona.css">
+    <link rel="stylesheet" href="css/css/como-funciona.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.css">
     <title>Como funciona?</title>
 </head>
@@ -27,14 +27,30 @@
         </div>
         <div class="sections-container">
             <div class="section">
-                <h3 class="title-visita">VISITA DO VETERINÁRIO</h3>
+                <div class="title-visita-container">
+                    <div class="icon-container">
+                        <i class="fas fa-house"></i>
+                    </div>
+                    <h3 class="title-visita">VISITA DO VETERINÁRIO</h3>
+                </div>
                 <p class="text-visita">No dia agendado, nosso veterinário irá até sua residência com todos os equipamentos necessários para realizar a vacinação do seu pet.</p>
             </div>
             <div class="separator"></div>
-            <div class="section">
-                <h3 class="title-acompanhamento">ACOMPANHAMENTO PÓS-ATENDIMENTO</h3>
+            <div class="sections">
+                <div class="title-acompanhamento-container">
+                    <div class="icon-container2">
+                        <i class="fas fa-message"></i>
+                    </div>
+                    <h3 class="title-acompanhamento">ACOMPANHAMENTO<br> PÓS-ATENDIMENTO</h3>
+                </div>
                 <p class="text-acompanhamento">Após a visita, você terá acesso a um canal direto de comunicação com o veterinário para esclarecer dúvidas e receber orientações adicionais, garantindo o cuidado contínuo com seu pet.</p>
             </div>
+        </div>
+        <div class="agende-agora">
+            <h2 class="title-2">Não espere mais para proporcionar<br> o melhor cuidado ao seu animal!</h2>
+            <p class="text-agendeja"> Agende já a visita do nosso veterinário e garanta a saúde e bem-estar do<br> seu pet com toda a comodidade e segurança do atendimento a domicílio.</p>
+            <a href="cadastro.php"><button class="btn btn-primary btn-custom">AGENDE AGORA</button></a>
+
         </div>
     </div>
 </body>
