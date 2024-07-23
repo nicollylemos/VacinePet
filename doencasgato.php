359fc541e3ae75fb2d1b74c1e3151f278ab9eb84
rafamans4no <rafaela.fernandes48@etec.sp.gov.br>
2024-07-23 19:50:39 -0300

    2307

--- doencasgato.php
+++ doencasgato.php
@@ -1,14 +1,54 @@
-<?php 
- include("./inc/header.php")
+<?php
+include ("./inc/header.php")
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="UTF-8">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=10, minimum-scale=1.0">
+    <link rel="stylesheet"href="css/css/doencasgato.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.css">
+    <link rel="stylesheet" href="css/swiper/swiper-bundle.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <title>Doenças Felinas</title>
 </head>
 <body>
-    
+    <section class="start">
+        <div class="espaco"></div>
+        <div class="escritos">
+            <h1 class="title-start">DOENÇAS<br>FELINAS</h1>
+            <h4 class="sub-title-start">Conheça as doenças felinas e proteja seu melhor amigo!<br>
+                Saber mais sobre infecções e problemas genéticos<br>
+                é essencial para a saúde do seu gato. Identifique<br>
+                sintomas cedo e assegure um tratamento eficaz.<br>
+                Previna doenças com vacinação e cuidados regulares.<br>
+                Informe-se e fortaleça o vínculo com seu pet.<br>
+                Gostaria de conhecer mais sobre as doenças felinas?</h4>
+            <button onclick="scrollToSection()" type="submit" class="btn-start">CONHECER</button>
+            <div class="splash-container">
+            </div>
+        </div>
+        <!--bolinhas laranjas -->
+            <div class="orange-balls"></div>
+            <div class="orange-balls1"></div>
+            <div class="orange-balls2"></div>
+            <!--bolinhas azuis -->
+            <div class="blue-balls"></div>
+            <div class="blue-balls1"></div>
+            <div class="blue-balls2"></div>
+            <div class="blue-balls3"></div>
+            <!--bolinhas azuis meio -->
+            <div class="bolinha-furada"></div>
+            <div class="bolinha-furada1"></div>
+    </section>
+    <section id="doencas">
+    <!--carrossel help-->
+    </section>
 </body>
+<?php include ("./inc/footer.php") ?>
+<!--scroll JS-->
+<script src="js/scroll.js"></script>
+<!--swiper JS -->
+<script src="js/swiper/swiper-bundle.min.js"></script>
 </html>
