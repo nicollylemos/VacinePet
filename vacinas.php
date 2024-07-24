fb3becf7048a8b16e440da25322270c928f4b22f
EduardaOS <dudaesouza26@gmail.com>
2024-07-24 12:07:05 -0300

    inicio sobre as vacinas

---
+++ vacinas.php
@@ -0,0 +1,26 @@
+<?php include ("./inc/header.php") ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="css/css/littlevacinas.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.css">
+    <title>Vacina Polivalente</title>
+
+</head>
+
+<body>
+    <div class="top-section">
+       <h1 clas="title-vacina">POLIVALENTE</h1>
+    </div>
+    <div class="bottom-section"> </div>
+</body>
+
+<?php include ("./inc/footer.php") ?>
+<!--Swiper JavaScript-->
+<script src="js/swiper/swiper-bundle.min.js"></script>
+
+</html>
