8eabdb57251a1d1db6154f39ebc5ca63ac2c6a3f
rafamans4no <rafaela.fernandes48@etec.sp.gov.br>
2024-07-25 17:04:30 -0300

    2507

--- doencascachorro.php
+++ doencascachorro.php
@@ -57,7 +57,12 @@ include ("./inc/header.php")
                             <h3 class="nome-vacina">
                                 <!-- futuro php-->CINOMOSE
                             </h3>
-                            <p class="description">A cinomose é um dos maiores pesadelos dos tutores de cães, é uma doença altamente infecciosa e frequentemente fatal causada por um vírus. A transmissão ocorre pelo contato direto com secreções nasais e oculares, tosse e espirros de um outro animal infectado... </p><a href="#"><button type="submit"
+                            <div class="textinhos">
+                                <p class="description">A cinomose é um dos maiores pesadelos dos tutores de cães, é uma doença altamente infecciosa e frequentemente 
+                                    fatal causada por um vírus. A transmissão ocorre pelo contato direto com secreções nasais e oculares, 
+                                    tosse e espirros de um outro animal infectado... </p>
+                            </div>
+                            <a href="#"><button type="submit"
                                 class="btn-vacinas" ><i class="fa-solid fa-angle-down" style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button></a>
                         </div>
                     </div>
@@ -66,7 +71,10 @@ include ("./inc/header.php")
                             <h3 class="nome-vacina">
                                 <!-- futuro php-->LEPTOSPIROSE CANINA
                             </h3>
-                            <p class="description">A leptospirose canina é uma doença com alto índice de mortalidade, causada por bactérias que também podem infectar os humanos. A transmissão se dá principalmente pelo contato com a urina... </p><button type="submit"
+                            <div class="textinhos">
+                                <p class="description">A leptospirose canina é uma doença com alto índice de mortalidade, causada por bactérias que também podem infectar os humanos. A transmissão se dá principalmente pelo contato com a urina... 
+                            </div>
+                            </p><button type="submit"
                                 class="btn-vacinas"><i class="fa-solid fa-angle-down" style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button>
                         </div>
                     </div>
@@ -75,7 +83,12 @@ include ("./inc/header.php")
                             <h3 class="nome-vacina">
                                 <!-- futuro php-->CORONAVIROSE
                             </h3>
-                            <p class="description">A coronavirose é uma doença viral, altamente contagiosa, que acomete os intestinos, provocando um quadro de diarreia e vômito. É muito comum em locais em que grande número de animais fica abrigado... </p><button type="submit"
+                            <div class="textinhos">
+                                <p class="description">Aqui está o texto combinado:
+
+A coronavirose canina é uma doença viral altamente contagiosa que acomete principalmente os intestinos, causando diarreia e vômito. Ela afeta frequentemente animais mais jovens e pode também comprometer o sistema respiratório. Um dos principais...
+                            </div>
+                            </p><button type="submit"
                                 class="btn-vacinas"><i class="fa-solid fa-angle-down" style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button>
                         </div>
                     </div>
@@ -84,7 +97,10 @@ include ("./inc/header.php")
                             <h3 class="nome-vacina">
                                 <!-- futuro php-->PARVOVIROSE
                             </h3>
-                            <p class="description">A parvovirose é uma doença causada por um vírus que ataca o aparelho digestivo e o sistema imunológico dos cães, principalmente em filhotes. Sua transmissão se dá pelo contato direto com as fezes de animais doentes... </p><button type="submit"
+                            <div class="textinhos">
+                                <p class="description">A parvovirose canina (ou parvovírus canino) é uma doença viral altamente contagiosa que afeta os cães, especialmente os filhotes. Ela ataca o aparelho digestivo e o sistema imunológico dos cães. A transmissão ocorre através do contato direto com fezes ou vômitos infectados...
+                            </div>
+                            </p><button type="submit"
                                 class="btn-vacinas"><i class="fa-solid fa-angle-down" style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button>
                         </div>
                     </div>
@@ -93,7 +109,10 @@ include ("./inc/header.php")
                             <h3 class="nome-vacina">
                                 <!-- futuro php-->HEPATITE INFECCIOSA 
                             </h3>
-                            <p class="description">A hepatite infecciosa canina é uma doença aguda que acomete o fígado, causada por um vírus agressivo, transmitida pelas descargas orais e nasais, fezes e pelo contato com o ambiente... </p><button type="submit"
+                            <div class="textinhos">
+                                <p class="description">A hepatite infecciosa canina é uma doença aguda que acomete o fígado, causada por um vírus agressivo, transmitida pelas descargas orais e nasais, fezes e pelo contato com o ambiente... 
+                            </div>
+                            </p><button type="submit"
                                 class="btn-vacinas"><i class="fa-solid fa-angle-down" style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button>
                         </div>
                     </div>
@@ -102,7 +121,10 @@ include ("./inc/header.php")
                             <h3 class="nome-vacina">
                                 <!-- futuro php-->ADENOVIROSE RESPIRATÓRIA
                             </h3>
-                            <p class="description">O adenovírus canino respiratório é altamente contagioso e, se não tratado, pode levar a complicações como as pneumonias. A transmissão se dá pelo contato direto entre cães, onde os animais infectados transmitem a doença para os cães sadios... </p><button type="submit"
+                            <div class="textinhos">
+                                <p class="description">O adenovírus canino respiratório é altamente contagioso e, se não tratado, pode levar a complicações como as pneumonias. A transmissão se dá pelo contato direto entre cães, onde os...
+                            </div>
+                            </p><button type="submit"
                                 class="btn-vacinas"><i class="fa-solid fa-angle-down" style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button>
                         </div>
                     </div>
@@ -111,7 +133,10 @@ include ("./inc/header.php")
                             <h3 class="nome-vacina">
                                 <!-- futuro php-->PARAINFLUENZA 
                             </h3>
-                            <p class="description">O vírus da parainfluenza é um parente do vírus da gripe humana. A transmissão acontece quando animais doentes tossem, espirram ou quando um cão saudável tem contato com objetos de um animal doente... </p><button type="submit"
+                            <div class="textinhos">
+                                <p class="description">O vírus da parainfluenza é um parente do vírus da gripe humana. A transmissão acontece quando animais doentes tossem, espirram ou quando um cão saudável tem contato com objetos de um animal doente... 
+                            </div>
+                            </p><button type="submit"
                                 class="btn-vacinas"><i class="fa-solid fa-angle-down" style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button>
                         </div>
                     </div>
@@ -120,7 +145,10 @@ include ("./inc/header.php")
                             <h3 class="nome-vacina">
                                 <!-- futuro php-->TOSSE DOS CANIS  
                             </h3>
-                            <p class="description">A tosse dos canis, também conhecida como gripe canina ou traqueobronquite infecciosa canina, é uma doença altamente contagiosa, causada pela bactéria Bordetella bronchiseptica, também podendo ter... </p><button type="submit"
+                            <div class="textinhos">
+                                <p class="description">A tosse dos canis, também conhecida como gripe canina ou traqueobronquite infecciosa canina, é uma doença altamente contagiosa, causada pela bactéria Bordetella bronchiseptica, também podendo ter... 
+                            </div>
+                            </p><button type="submit"
                                 class="btn-vacinas"><i class="fa-solid fa-angle-down" style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button>
                         </div>
                     </div>
@@ -129,7 +157,10 @@ include ("./inc/header.php")
                             <h3 class="nome-vacina">
                                 <!-- futuro php-->RAIVA 
                             </h3>
-                            <p class="description">A raiva é uma das doenças infecciosas de cães e gatos mais importantes porque é uma zoonose fatal. Geralmente é transmitida por meio da mordida de um animal infectado (que pode ser um cão ou um gato) ou de animais que funcionam como reservatórios naturais (morcegos, por exemplo)... </p><button type="submit"
+                            <div class="textinhos">
+                                <p class="description">A raiva é uma das doenças infecciosas de cães e gatos mais importantes porque é uma zoonose fatal. Geralmente é transmitida por meio da mordida de um animal infectado (que pode ser um cão ou um gato) ou... 
+                            </div>
+                            </p><button type="submit"
                                 class="btn-vacinas"><i class="fa-solid fa-angle-down" style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button>
                         </div>
                     </div>
@@ -138,7 +169,10 @@ include ("./inc/header.php")
                             <h3 class="nome-vacina">
                                 <!-- futuro php-->GIARDÍASE
                             </h3>
-                            <p class="description">A giardíase é uma doença causada por um parasita, o protozoário Giardia lamblia, que vive no intestino delgado dos cães. É altamente contagiosa. Pode ser transmitida dos cães para outros animais domésticos e até mesmo para o ser humano... </p><button type="submit"
+                            <div class="textinhos">
+                                <p class="description">A giardíase é uma doença causada por um parasita, o protozoário Giardia lamblia, que vive no intestino delgado dos cães. É altamente contagiosa. Pode ser transmitida dos cães para outros animais domésticos...
+                            </div>
+                        </p><button type="submit"
                                 class="btn-vacinas"><i class="fa-solid fa-angle-down" style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button>
                         </div>
                     </div>
