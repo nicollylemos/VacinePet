e549fc32c76ecba9a57f9fb9cdff412f62c7ddd5
rafamans4no <rafaela.fernandes48@etec.sp.gov.br>
2024-07-25 16:42:52 -0300

    2507

--- doencascachorro.php
+++ doencascachorro.php
@@ -8,7 +8,7 @@ include ("./inc/header.php")
     <meta charset="UTF-8">
     <meta name="viewport"
         content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=10, minimum-scale=1.0">
-    <link rel="stylesheet" href="css/css/doencacao.css">
+    <link rel="stylesheet" href="css/css/rafa.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.css">
     <link rel="stylesheet" href="css/swiper/swiper-bundle.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
@@ -45,7 +45,107 @@ include ("./inc/header.php")
         <div class="bolinha-furada1"></div>
     </section>
     <section id="doencas">
-        <!--carrossel help-->
+        <div class="space"></div>
+        <div class="titulo-vacina">
+            <h2>Doenças Caninas</h2>
+        </div>
+    <div class="slide-container swiper">
+            <div class="slide-content">
+                <div class=" card-wrapper swiper-wrapper">
+                    <div class="card swiper-slide">
+                        <div class="card-content">
+                            <h3 class="nome-vacina">
+                                <!-- futuro php-->CINOMOSE
+                            </h3>
+                            <p class="description">A cinomose é um dos maiores pesadelos dos tutores de cães, é uma doença altamente infecciosa e frequentemente fatal causada por um vírus. A transmissão ocorre pelo contato direto com secreções nasais e oculares, tosse e espirros de um outro animal infectado... </p><a href="#"><button type="submit"
+                                class="btn-vacinas" ><i class="fa-solid fa-angle-down" style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button></a>
+                        </div>
+                    </div>
+                    <div class=" card swiper-slide">
+                        <div class="card-content">
+                            <h3 class="nome-vacina">
+                                <!-- futuro php-->LEPTOSPIROSE CANINA
+                            </h3>
+                            <p class="description">A leptospirose canina é uma doença com alto índice de mortalidade, causada por bactérias que também podem infectar os humanos. A transmissão se dá principalmente pelo contato com a urina... </p><button type="submit"
+                                class="btn-vacinas"><i class="fa-solid fa-angle-down" style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button>
+                        </div>
+                    </div>
+                    <div class=" card swiper-slide">
+                        <div class="card-content">
+                            <h3 class="nome-vacina">
+                                <!-- futuro php-->CORONAVIROSE
+                            </h3>
+                            <p class="description">A coronavirose é uma doença viral, altamente contagiosa, que acomete os intestinos, provocando um quadro de diarreia e vômito. É muito comum em locais em que grande número de animais fica abrigado... </p><button type="submit"
+                                class="btn-vacinas"><i class="fa-solid fa-angle-down" style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button>
+                        </div>
+                    </div>
+                    <div class=" card swiper-slide">
+                        <div class="card-content">
+                            <h3 class="nome-vacina">
+                                <!-- futuro php-->PARVOVIROSE
+                            </h3>
+                            <p class="description">A parvovirose é uma doença causada por um vírus que ataca o aparelho digestivo e o sistema imunológico dos cães, principalmente em filhotes. Sua transmissão se dá pelo contato direto com as fezes de animais doentes... </p><button type="submit"
+                                class="btn-vacinas"><i class="fa-solid fa-angle-down" style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button>
+                        </div>
+                    </div>
+                    <div class="card swiper-slide">
+                        <div class="card-content">
+                            <h3 class="nome-vacina">
+                                <!-- futuro php-->HEPATITE INFECCIOSA 
+                            </h3>
+                            <p class="description">A hepatite infecciosa canina é uma doença aguda que acomete o fígado, causada por um vírus agressivo, transmitida pelas descargas orais e nasais, fezes e pelo contato com o ambiente... </p><button type="submit"
+                                class="btn-vacinas"><i class="fa-solid fa-angle-down" style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button>
+                        </div>
+                    </div>
+                    <div class="card swiper-slide">
+                        <div class="card-content">
+                            <h3 class="nome-vacina">
+                                <!-- futuro php-->ADENOVIROSE RESPIRATÓRIA
+                            </h3>
+                            <p class="description">O adenovírus canino respiratório é altamente contagioso e, se não tratado, pode levar a complicações como as pneumonias. A transmissão se dá pelo contato direto entre cães, onde os animais infectados transmitem a doença para os cães sadios... </p><button type="submit"
+                                class="btn-vacinas"><i class="fa-solid fa-angle-down" style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button>
+                        </div>
+                    </div>
+                    <div class="card swiper-slide">
+                        <div class="card-content">
+                            <h3 class="nome-vacina">
+                                <!-- futuro php-->PARAINFLUENZA 
+                            </h3>
+                            <p class="description">O vírus da parainfluenza é um parente do vírus da gripe humana. A transmissão acontece quando animais doentes tossem, espirram ou quando um cão saudável tem contato com objetos de um animal doente... </p><button type="submit"
+                                class="btn-vacinas"><i class="fa-solid fa-angle-down" style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button>
+                        </div>
+                    </div>
+                    <div class="card swiper-slide">
+                        <div class="card-content">
+                            <h3 class="nome-vacina">
+                                <!-- futuro php-->TOSSE DOS CANIS  
+                            </h3>
+                            <p class="description">A tosse dos canis, também conhecida como gripe canina ou traqueobronquite infecciosa canina, é uma doença altamente contagiosa, causada pela bactéria Bordetella bronchiseptica, também podendo ter... </p><button type="submit"
+                                class="btn-vacinas"><i class="fa-solid fa-angle-down" style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button>
+                        </div>
+                    </div>
+                    <div class="card swiper-slide">
+                        <div class="card-content">
+                            <h3 class="nome-vacina">
+                                <!-- futuro php-->RAIVA 
+                            </h3>
+                            <p class="description">A raiva é uma das doenças infecciosas de cães e gatos mais importantes porque é uma zoonose fatal. Geralmente é transmitida por meio da mordida de um animal infectado (que pode ser um cão ou um gato) ou de animais que funcionam como reservatórios naturais (morcegos, por exemplo)... </p><button type="submit"
+                                class="btn-vacinas"><i class="fa-solid fa-angle-down" style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button>
+                        </div>
+                    </div>
+                    <div class="card swiper-slide">
+                        <div class="card-content">
+                            <h3 class="nome-vacina">
+                                <!-- futuro php-->GIARDÍASE
+                            </h3>
+                            <p class="description">A giardíase é uma doença causada por um parasita, o protozoário Giardia lamblia, que vive no intestino delgado dos cães. É altamente contagiosa. Pode ser transmitida dos cães para outros animais domésticos e até mesmo para o ser humano... </p><button type="submit"
+                                class="btn-vacinas"><i class="fa-solid fa-angle-down" style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-button-prev"></div>
+                <div class="swiper-button-next"></div>
+            </div>
     </section>
 </body>
 <?php include ("./inc/footer.php") ?>
@@ -53,5 +153,7 @@ include ("./inc/header.php")
 <script src="js/scroll.js"></script>
 <!--swiper JS -->
 <script src="js/swiper/swiper-bundle.min.js"></script>
+<!--JS -->
+<script src="js/telainicial.js"></script>
 
 </html>
