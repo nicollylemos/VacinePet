29abf62f22479e28a837ace86be2467dabeffc1b
rafamans4no <rafaela.fernandes48@etec.sp.gov.br>
2024-07-24 21:18:55 -0300

    2407

--- doencascachorro.php
+++ doencascachorro.php
@@ -1,18 +1,20 @@
 <?php
 include ("./inc/header.php")
-?>
+    ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport"
         content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=10, minimum-scale=1.0">
-    <link rel="stylesheet"href="css/css/doencacachorro.css">
+    <link rel="stylesheet" href="css/css/auau.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.css">
     <link rel="stylesheet" href="css/swiper/swiper-bundle.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <title>Doenças Caninas</title>
 </head>
+
 <body>
     <section class="start">
         <div class="espaco"></div>
@@ -30,20 +32,20 @@ include ("./inc/header.php")
             </div>
         </div>
         <!--bolinhas laranjas -->
-            <div class="orange-balls"></div>
-            <div class="orange-balls1"></div>
-            <div class="orange-balls2"></div>
-            <!--bolinhas azuis -->
-            <div class="blue-balls"></div>
-            <div class="blue-balls1"></div>
-            <div class="blue-balls2"></div>
-            <div class="blue-balls3"></div>
-            <!--bolinhas azuis meio -->
-            <div class="bolinha-furada"></div>
-            <div class="bolinha-furada1"></div>
+        <div class="orange-balls"></div>
+        <div class="orange-balls1"></div>
+        <div class="orange-balls2"></div>
+        <!--bolinhas azuis -->
+        <div class="blue-balls"></div>
+        <div class="blue-balls1"></div>
+        <div class="blue-balls2"></div>
+        <div class="blue-balls3"></div>
+        <!--bolinhas azuis meio -->
+        <div class="bolinha-furada"></div>
+        <div class="bolinha-furada1"></div>
     </section>
     <section id="doencas">
-    <!--carrossel help-->
+        <!--carrossel help-->
     </section>
 </body>
 <?php include ("./inc/footer.php") ?>
@@ -51,4 +53,5 @@ include ("./inc/header.php")
 <script src="js/scroll.js"></script>
 <!--swiper JS -->
 <script src="js/swiper/swiper-bundle.min.js"></script>
+
 </html>
