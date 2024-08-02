f94718780b0694cb80aa8252c813c422148bc0ea
EduardaOS <dudaesouza26@gmail.com>
2024-08-01 22:20:52 -0300

    iniciando responsividade

--- agendamento.php
+++ agendamento.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/css/responsividade/agendamento.css">
     <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
     <link rel="stylesheet" href="css/css/agendamento.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.css">
