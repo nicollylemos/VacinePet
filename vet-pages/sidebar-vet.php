132dce9b5ce7bb9fab4c2b65ed8fffc190e39683
nicollylemos <nicollylemos752@gmail.com>
2024-08-09 10:34:59 -0300

    n tanko mais esse tcc

--- vet-pages/sidebar-vet.php
+++ vet-pages/sidebar-vet.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../css/css/sidebar-vet.css">
+    <link rel="stylesheet" href="../css/css/sidebarvet.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.css">
 </head>
 
@@ -25,64 +25,30 @@
                     <ul class="menu-links">
                         <li class="nav-link">
                             <a href="sobre-vacinepet.php">
-                                <i class="fa-solid fa-syringe icon"></i>
-                                <span class="text nav-text">Sobre o VacinePet</span>
+                                <i class="fa-solid fa-bars-progress icon"></i>
+                                <span class="text nav-text">Agendamentos Ativos</span>
                             </a>
                         </li>
 
                         <li class="nav-link">
                             <a href="sobre-profissional.php">
-                                <i class="fa-solid fa-user-tie icon"></i>
-                                <span class="text nav-text">Sobre a profissional</span>
+                                <i class="fa-solid fa-clock-rotate-left icon"></i>
+                                <span class="text nav-text">Históricos de Agendamentos</span>
                             </a>
                         </li>
 
                         <li class="nav-link">
-                            <a href="sobre-atend.php">
-                                <i class="fa-solid fa-bell-concierge icon"></i>
-                                <span class="text nav-text">Sobre o atendimento</span>
+                            <a href="diary.php">
+                                <i class="fa-solid fa-calendar-days icon"></i>
+                                <span class="text nav-text">Meu Diário</span>
                             </a>
                         </li>
                     </ul>
                     <ul class="menu-links">
                         <li class="nav-link">
                             <a href="#">
-                                <i class="fa-solid fa-shield-halved icon"></i>
-                                <span class="text nav-text">Políticas de Privacidade</span>
-                            </a>
-                        </li>
-
-                        <li class="nav-link">
-                            <a href="#">
-                                <i class="fa-solid fa-file-signature icon"></i>
-                                <span class="text nav-text">Termos de uso</span>
-                            </a>
-                        </li>
-
-                        <li class="nav-link">
-                            <a href="#">
-                                <i class="fa-solid fa-copyright icon"></i>
-                                <span class="text nav-text">Direitos Autorais</span>
-                            </a>
-                        </li>
-                    </ul>
-                    <ul class="menu-links">
-                        <li class="nav-link">
-                            <a href="#">
-                                <i class="fa-solid fa-house icon"></i>
-                                <span class="text nav-text"> Vacinação em domicílio</span>
-                            </a>
-                        </li>
-                        <li class="nav-link">
-                            <a href="#">
-                                <i class="fa-regular fa-calendar-days icon"></i>
-                                <span class="text nav-text">Confirmar agendamento</span>
-                            </a>
-                        </li>
-                        <li class="nav-link">
-                            <a href="pagamento.php">
-                                <i class="fa-solid fa-cash-register icon"></i>
-                                <span class="text nav-text">Formas de Pagamento</span>
+                                <i class="fa-solid fa-money-bill icon"></i>
+                                <span class="text nav-text">Alteração de Valores</span>
                             </a>
                         </li>
                     </ul>
