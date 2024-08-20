8696565285f3db8e586554a121ae35c82b5f2651
EduardaOS <dudaesouza26@gmail.com>
2024-08-20 15:27:05 -0300

    2008

---
+++ nav.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Navbar Animação em Bolha</title>
+
+    <link rel="stylesheet" href="css/css/nav.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.css">
+
+    <script defer src="js/header.js"></script>
+
+</head>
+
+<body>
+    <header class="header">
+        <nav class="nav">
+            <a href="index.php" class="logo">VacinePet<i class="fa-solid fa-paw fa-rotate-by"
+                    style="color: #FA5909; --fa-rotate-angle: 49deg; width: 20px; height: 20px; font-size: 15px;"></i></a>
+            <button class="hamburger"></button>
+            <ul class="nav-list">
+                <li><a href="index.php">Início</a></li>
+                <li><a href="agendamento.php">Agendamento</a></li>
+                <li><a href="sobre-vacinepet.php">Sobre</a></li>
+                <li><a href="sobre-vacinepet.php">Profissional</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+
+</html>
