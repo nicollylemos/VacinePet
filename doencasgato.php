06b0d7933bc70bfe4ccef39f3db380ae49046275
EduardaOS <dudaesouza26@gmail.com>
2024-08-27 22:37:02 -0300

    doenças gato e nav bar feitos

--- doencasgato.php
+++ doencasgato.php
@@ -1,5 +1,5 @@
 <?php
-include("./inc/teste-nav.php");
+include("./inc/header.php");
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -19,7 +19,7 @@ include("./inc/teste-nav.php");
     <section class="start">
         <div class="espaco"></div>
         <div class="escritos">
-            <h1 class="title-start">DOENÇAS<br>FELINAS</h1>
+        <h1 class="title-start">DOENÇAS<br>FELINAS</h1>
             <h4 class="sub-title-start">Conheça as doenças felinas e proteja seu melhor amigo!<br>
                 Saber mais sobre infecções e problemas genéticos<br>
                 é essencial para a saúde do seu gato. Identifique<br>
@@ -34,7 +34,168 @@ include("./inc/teste-nav.php");
 
     </section>
     <section id="doencas">
-        <!--carrossel help-->
+        <div class="space"></div>
+        <div class="titulo-vacina">
+            <h2>DOENÇAS FELINAS</h2>
+            <p class="subtitle">Conheça as principais doenças felinas e agende a vacinação com o veterinário<br> para garantir a saúde e bem-estar do seu pet.</p>
+        </div>
+        <div class="slide-container swiper">
+            <div class="slide-content">
+                <div class=" card-wrapper swiper-wrapper">
+                    <div class="card swiper-slide">
+                        <div class="card-content">
+                            <h3 class="nome-vacina">
+                                <!-- futuro php-->CINOMOSE
+                            </h3>
+                            <div class="textinhos">
+                                <p class="description">A cinomose é um dos maiores pesadelos dos tutores de cães, é uma
+                                    doença altamente infecciosa e frequentemente
+                                    fatal causada por um vírus. A transmissão ocorre pelo contato direto com secreções
+                                    nasais e oculares,
+                                    tosse e espirros de um outro animal infectado... </p>
+                            </div>
+                            <a href="#"><button type="submit" class="btn-vacinas"><i class="fa-solid fa-angle-down"
+                                        style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button></a>
+                        </div>
+                    </div>
+                    <div class=" card swiper-slide">
+                        <div class="card-content">
+                            <h3 class="nome-vacina">
+                                <!-- futuro php-->LEPTOSPIROSE CANINA
+                            </h3>
+                            <div class="textinhos">
+                                <p class="description">A leptospirose canina é uma doença com alto índice de
+                                    mortalidade, causada por bactérias que também podem infectar os humanos. A
+                                    transmissão se dá principalmente pelo contato com a urina...
+                            </div>
+                            </p><button type="submit" class="btn-vacinas"><i class="fa-solid fa-angle-down"
+                                    style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button>
+                        </div>
+                    </div>
+                    <div class=" card swiper-slide">
+                        <div class="card-content">
+                            <h3 class="nome-vacina">
+                                <!-- futuro php-->CORONAVIROSE
+                            </h3>
+                            <div class="textinhos">
+                                <p class="description">Aqui está o texto combinado:
+
+                                    A coronavirose canina é uma doença viral altamente contagiosa que acomete
+                                    principalmente os intestinos, causando diarreia e vômito. Ela afeta frequentemente
+                                    animais mais jovens e pode também comprometer o sistema respiratório. Um dos
+                                    principais...
+                            </div>
+                            </p><button type="submit" class="btn-vacinas"><i class="fa-solid fa-angle-down"
+                                    style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button>
+                        </div>
+                    </div>
+                    <div class=" card swiper-slide">
+                        <div class="card-content">
+                            <h3 class="nome-vacina">
+                                <!-- futuro php-->PARVOVIROSE
+                            </h3>
+                            <div class="textinhos">
+                                <p class="description">A parvovirose canina (ou parvovírus canino) é uma doença viral
+                                    altamente contagiosa que afeta os cães, especialmente os filhotes. Ela ataca o
+                                    aparelho digestivo e o sistema imunológico dos cães. A transmissão ocorre através do
+                                    contato direto com fezes ou vômitos infectados...
+                            </div>
+                            </p><button type="submit" class="btn-vacinas"><i class="fa-solid fa-angle-down"
+                                    style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button>
+                        </div>
+                    </div>
+                    <div class="card swiper-slide">
+                        <div class="card-content">
+                            <h3 class="nome-vacina">
+                                <!-- futuro php-->HEPATITE INFECCIOSA
+                            </h3>
+                            <div class="textinhos">
+                                <p class="description">A hepatite infecciosa canina é uma doença aguda que acomete o
+                                    fígado, causada por um vírus agressivo, transmitida pelas descargas orais e nasais,
+                                    fezes e pelo contato com o ambiente...
+                            </div>
+                            </p><button type="submit" class="btn-vacinas"><i class="fa-solid fa-angle-down"
+                                    style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button>
+                        </div>
+                    </div>
+                    <div class="card swiper-slide">
+                        <div class="card-content">
+                            <h3 class="nome-vacina">
+                                <!-- futuro php-->ADENOVIROSE RESPIRATÓRIA
+                            </h3>
+                            <div class="textinhos">
+                                <p class="description">O adenovírus canino respiratório é altamente contagioso e, se não
+                                    tratado, pode levar a complicações como as pneumonias. A transmissão se dá pelo
+                                    contato direto entre cães, onde os...
+                            </div>
+                            </p><button type="submit" class="btn-vacinas"><i class="fa-solid fa-angle-down"
+                                    style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button>
+                        </div>
+                    </div>
+                    <div class="card swiper-slide">
+                        <div class="card-content">
+                            <h3 class="nome-vacina">
+                                <!-- futuro php-->PARAINFLUENZA
+                            </h3>
+                            <div class="textinhos">
+                                <p class="description">O vírus da parainfluenza é um parente do vírus da gripe humana. A
+                                    transmissão acontece quando animais doentes tossem, espirram ou quando um cão
+                                    saudável tem contato com objetos de um animal doente...
+                            </div>
+                            </p><button type="submit" class="btn-vacinas"><i class="fa-solid fa-angle-down"
+                                    style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button>
+                        </div>
+                    </div>
+                    <div class="card swiper-slide">
+                        <div class="card-content">
+                            <h3 class="nome-vacina">
+                                <!-- futuro php-->TOSSE DOS CANIS
+                            </h3>
+                            <div class="textinhos">
+                                <p class="description">A tosse dos canis, também conhecida como gripe canina ou
+                                    traqueobronquite infecciosa canina, é uma doença altamente contagiosa, causada pela
+                                    bactéria Bordetella bronchiseptica, também podendo ter...
+                            </div>
+                            </p><button type="submit" class="btn-vacinas"><i class="fa-solid fa-angle-down"
+                                    style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button>
+                        </div>
+                    </div>
+                    <div class="card swiper-slide">
+                        <div class="card-content">
+                            <h3 class="nome-vacina">
+                                <!-- futuro php-->RAIVA
+                            </h3>
+                            <div class="textinhos">
+                                <p class="description">A raiva é uma das doenças infecciosas de cães e gatos mais
+                                    importantes porque é uma zoonose fatal. Geralmente é transmitida por meio da mordida
+                                    de um animal infectado (que pode ser um cão ou um gato) ou...
+                            </div>
+                            </p><button type="submit" class="btn-vacinas"><i class="fa-solid fa-angle-down"
+                                    style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button>
+                        </div>
+                    </div>
+                    <div class="card swiper-slide">
+                        <div class="card-content">
+                            <h3 class="nome-vacina">
+                                <!-- futuro php-->GIARDÍASE
+                            </h3>
+                            <div class="textinhos">
+                                <p class="description">A giardíase é uma doença causada por um parasita, o
+                                    protozoário Giardia lamblia, que vive no intestino delgado dos cães. É altamente
+                                    contagiosa. Pode ser transmitida dos cães para outros animais domésticos...
+                            </div>
+                            </p><button type="submit" class="btn-vacinas"><i class="fa-solid fa-angle-down"
+                                    style=" width: 13px; height:7px; font-size:30px; margin-right: 16%;"></i></button>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-button-prev"></div>
+                <div class="swiper-button-next"></div>
+            </div>
+            <div class="agende-agora">
+            <a href="cadastro.php"><button class="btn btn-primary btn-custom">AGENDE AGORA</button></a>
+
+        </div>
     </section>
 </body>
 <?php include ("./inc/footer.php") ?>
@@ -42,5 +203,7 @@ include("./inc/teste-nav.php");
 <script src="js/scroll.js"></script>
 <!--swiper JS -->
 <script src="js/swiper/swiper-bundle.min.js"></script>
+<!--JS -->
+<script src="js/telainicial.js"></script>
 
 </html>
--- inc/header.php
+++ inc/header.php
@@ -5,124 +5,175 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <style>
-    body {
-        margin: 0;
-        font-family: Arial, Helvetica, sans-serif;
-    }
-
-    .topnav {
-        overflow: hidden;
-        background-color: #fff;
-        border-bottom: 1px solid #ddd;
-    }
-
-    .topnav a {
-        float: left;
-        display: block;
-        color: #3e3e3e;
-        text-align: center;
-        padding: 14px 16px;
-        text-decoration: none;
-        font-size: 17px;
-    }
-
-    .topnav a:hover {
-        background-color: #ddd;
-        color: black;
-    }
-
-    .topnav a.active {
-        background-color: #04AA6D;
-        color: white;
-    }
-
-    .topnav .icon {
-        display: none;
-    }
-
-    .topnav .cad {
-        background-color: transparent;
-        border: 3.5px solid var(--primary-clr);
-        color: var(--primary-clr);
-        font-weight: 600;
-        border-radius: 8px;
-        width: 150px;
-        font-size: 15px;
-        cursor: pointer;
-        height: 35px;
-    }
-
-    .topnav button {
-        float: left;
-        display: block;
-        text-align: center;
-        margin: 14px 2px;
-        left: 55%;
-        position: relative;
-    }
-
-    .topnav .login {
-        color: #ffffff;
-        background-color: var(--primary-clr);
-        font-weight: 600;
-        border-radius: 8px;
-        width: 150px;
-        font-size: 15px;
-        cursor: pointer;
-        height: 35px;
-        border: none;
-    }
-
-
-    @media screen and (max-width: 600px) {
-        .topnav a:not(:first-child) {
-            display: none;
+        body {
+            margin: 0;
+            font-family: Arial, Helvetica, sans-serif;
         }
 
-        .topnav a.icon {
-            float: right;
-            display: block;
+        .topnav {
+            overflow: hidden;
+            background-color: white;
+            border-bottom: 1px solid #ddd;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 0 16px;
         }
-    }
 
-    @media screen and (max-width: 600px) {
-        .topnav.responsive {
+        .topnav .logo {
+            font-size: 24px;
+            font-family: baloo;
+            color: #FA5909;
+        }
+
+        .topnav .menu {
+            display: flex;
+            gap: 15px;
             position: relative;
         }
 
-        .topnav.responsive .icon {
-            position: absolute;
-            right: 0;
-            top: 0;
+        .topnav a {
+            display: block;
+            color: #3e3e3e;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+            font-size: 17px;
         }
 
-        .topnav.responsive a {
-            float: none;
-            display: block;
-            text-align: left;
+        .topnav a:hover {
+            color: #52BACB;
         }
-    }
+
+        .topnav .buttons {
+            display: flex;
+            gap: 1px;
+        }
+
+        .topnav .cad {
+            cursor: pointer;
+            font-size: 18px;
+            background-color: #ffffff;
+            border-radius: 10px;
+            border: 3.5px solid #52BACB;
+            width: 150px;
+            font-weight: 700;
+            letter-spacing: 0.5px;
+            text-decoration: none;
+            color: #52BACB;
+            font-family: league;
+            padding: 2%;
+        }
+
+        .topnav .login {
+            cursor: pointer;
+            font-size: 18px;
+            background-color: #ffffff;
+            border-radius: 10px;
+            border: 3.5px solid #52BACB;
+            width: 150px;
+            font-weight: 700;
+            letter-spacing: 0.5px;
+            text-decoration: none;
+            color: #52BACB;
+            font-family: league;
+            padding: 2%;
+        }
+
+        .topnav .icon {
+            display: none;
+        }
+
+        @media screen and (max-width: 600px) {
+            .topnav .menu a {
+                display: none;
+            }
+
+            .topnav .menu  {
+                display: block;
+            }
+
+            .topnav .buttons {
+                display: none;
+            }
+
+            .topnav .icon {
+                display: block;
+            }
+        }
+
+        @media screen and (max-width: 600px) {
+            .topnav.responsive {
+                position: relative;
+                flex-direction: column;
+                align-items: flex-start;
+                z-index: 80;
+            }
+
+            .topnav.responsive .menu {
+                display: block;
+                width: 100%;
+            }
+
+            .topnav.responsive .menu a {
+                display: block;
+                text-align: left;
+                width: 100%;
+                padding: 14px 16px;
+                box-sizing: border-box;
+            }
+
+            .topnav.responsive .buttons {
+                display: block;
+                width: 100%;
+                padding: 0 16px;
+            }
+
+            .topnav.responsive .buttons .login,
+            .topnav.responsive .buttons .cad {
+                width: auto;
+                margin: 5px 0;
+            }
+
+            .topnav.responsive .icon {
+                position: absolute;
+                right: 16px;
+                top: 16px;
+            }
+        }
     </style>
 </head>
 
 <body>
 
     <div class="topnav" id="myTopnav">
-        <a href="#home" class="active">Home</a>
-        <a href="#news">News</a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
+        <a href="index.php" class="logo">VacinePet<i class="fa-solid fa-paw fa-rotate-by"
+                style="color: #FA5909; --fa-rotate-angle: 49deg; width: 20px; height: 20px; font-size: 15px;"></i></a>
+        <div class="menu">
+            <a href="index.php" class="home active">Início</a>
+            <a href="agendamento.php">Agendamento</a>
+            <a href="infos-page/sobre-vacinepet.php">Sobre</a>
+            <a href="infos-page/sobre-profissional.php">Profissional</a>
+
+        </div>
+        <div class="buttons">
+            <a href="login.php"><button class="login">Entrar</button></a>
+            <a href="cadastro.php"><button class="cad">Cadastrar</button></a>
+        </div>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i> <!-- Aqui está o ícone de hambúrguer -->
+        </a>
     </div>
 
     <script>
-    function myFunction() {
-        var x = document.getElementById("myTopnav");
-        if (x.className === "topnav") {
-            x.className += " responsive";
-        } else {
-            x.className = "topnav";
+        function myFunction() {
+            var x = document.getElementById("myTopnav");
+            if (x.className === "topnav") {
+                x.className += " responsive";
+            } else {
+                x.className = "topnav";
+            }
         }
-    }
     </script>
 
 </body>
