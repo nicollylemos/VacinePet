2f2bb3a2075d748e5d53cc19adb5dc2a887792fd
rafamans4no <rafaela.fernandes48@etec.sp.gov.br>
2024-09-17 23:47:15 -0300

    1709

--- conteudo/castracao.php
+++ conteudo/castracao.php
@@ -7,42 +7,43 @@ include("../inc/header.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/css/castracaoo.css">
+    <link rel="stylesheet" href="../css/css/castracao.css">
+    <link rel="stylesheet" href="../css/css/responsividade/telacastracao.css">
     <link rel="stylesheet" href="../css/css/whatsapp.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.css">
     <title>Castração de Felinos</title>
 </head>
 
 <body>
-    <h1 id="title">CASTRAÇÃO DE FELINOS</h1>
-    <div class="text">
-        <h2 id="sub-title">O que é Castração?</h2>
-        <p>A castração é um procedimento cirúrgico que remove os órgãos reprodutivos dos felinos. Para os machos, são
-            removidos os testículos, e para as fêmeas, os ovários e o útero. Este procedimento oferece diversos
-            benefícios,
-            como a prevenção de doenças, a redução de comportamentos indesejados e o controle da população de
-            animais.<br></p>
+    <div class="resp">Como funciona a <br> castração de felinos?</div>
+    <div class="container"> <h1 id="title">CASTRAÇÃO DE FELINOS</h1>
+        <div class="text">
+            <h2 id="sub-title" class="sub">O que é Castração?</h2>
+            <p id="p1">A castração é um procedimento cirúrgico que remove os órgãos reprodutivos dos felinos. Para os machos,
+                são
+                removidos os testículos, e para as fêmeas, os ovários e o útero. Este procedimento oferece diversos
+                benefícios,
+                como a prevenção de doenças, a redução de comportamentos indesejados e o controle da população de
+                animais.<br></p>
 
-        <h2 id="sub-title">Especialização e Valor</h2>
-        <p>Nossa veterinária é especializada exclusivamente na castração de felinos. Não realizamos castração de cães. O
-            valor para a castração de felinos é de R$ 200,00. Este valor inclui o procedimento cirúrgico, anestesia e
-            cuidados pós-operatórios.<br></p>
-
-        <br>
-        <p>Para mais informações, entre em contato conosco. Estamos prontos para
-            oferecer o melhor cuidado para seu animal de estimação.</p>
+            <h2 id="sub-title" class="sub1">Especialização e Valor</h2>
+            <p id="p2">Nossa veterinária é especializada exclusivamente na castração de felinos. Não realizamos castração de
+                cães.
+                Para mais
+                informações e consulta de valores, entre em contato conosco. Estamos prontos para oferecer o melhor
+                cuidado para seu animal de estimação.</p>
 
 
+        </div>
+        <a href="agendamento.php"><button class="btn btn-primary btn-custom">Consulte Agora</button></a>
     </div>
-    <a href="agendamento.php"><button class="btn btn-primary btn-custom">AGENDE AGORA</button></a>
-
-    <!-- Ícone do WhatsApp -->
-    <div class="whatsapp-icon">
-        <a href="https://wa.me/5512991445347?text=Olá!%20Tenho%20uma%20dúvida%20sobre%20o%20serviço.%20Poderia%20me%20ajudar%20com%20mais%20informações?"
-            target="_blank" title="Fale conosco no WhatsApp">
-            <i class="fab fa-whatsapp"></i>
-        </a>
-    </div>
+        <!-- Ícone do WhatsApp -->
+        <div class="whatsapp-icon">
+            <a href="https://wa.me/5512991445347?text=Olá!%20Tenho%20uma%20dúvida%20sobre%20o%20serviço.%20Poderia%20me%20ajudar%20com%20mais%20informações?"
+                target="_blank" title="Fale conosco no WhatsApp">
+                <i class="fab fa-whatsapp"></i>
+            </a>
+        </div>
 
 
 
