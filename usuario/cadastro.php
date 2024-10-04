52e0e6be2e839d545b301f29362e9eeaaa8c0930
EduardaOS <dudaesouza26@gmail.com>
2024-10-04 18:07:46 -0300

    tchau e bença

--- usuario/cadastro.php
+++ usuario/cadastro.php
@@ -361,6 +361,8 @@ function mainPasswordValidate() {
 }
 </script>
 
+
+
 <script src="../js/cadastrar.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
