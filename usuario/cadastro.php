08547301d234658a33ebe53988b617a5837f2dc1
EduardaOS <dudaesouza26@gmail.com>
2024-10-08 17:04:39 -0300

    sim

--- usuario/cadastro.php
+++ usuario/cadastro.php
@@ -9,7 +9,7 @@ if (isset($_POST['submit'])) {
     $cpf = $_POST['tutor_cpf'];
     $telefone = $_POST['tutor_telefone'];
     $email = $_POST['tutor_email'];
-    $senha = $_POST['tutor_senha'];
+    $senha = password_hash ($_POST['tutor_senha'],PASSWORD_DEFAULT);
 
     $result = mysqli_query($conexao, "INSERT INTO tutor( nome, cpf, datanasc, telefone, email, senha) VALUES ('$nome', '$cpf', '$datanasc', '$telefone', '$email', '$senha')");
 
