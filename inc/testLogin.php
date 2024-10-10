269b086380f90c84ab44918b9207e25af6b1aeb5
EduardaOS <duadesouza26@gmail.com>
2024-10-10 16:43:48 -0300

    AAAAAAAAAAAAAAAAAAAAAAAA

--- inc/testLogin.php
+++ inc/testLogin.php
@@ -1,34 +1,33 @@
-<?php
-    session_start();
-    // print_r($_REQUEST);
-    if(isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha']))
-    {
-        // Acessa
-        include_once('../config.php');
-        $email = $_POST['email'];
-        $senha = $_POST['senha'];
+<?php 
+session_start();
 
-        // print_r('Email: ' . $email);
-        // print_r('<br>');
-        // print_r('Senha: ' . $senha);
-
-        $sql_code = "select * from tutor where email = '$email'  LIMIT 1";
-        $sql_exec = $conexao ->query($sql_code) or die($conexao->error);
-
-        $tutor = $sql_exec->fetch_assoc();
-
-        if(password_verify($senha, $tutor ['Senha'])){
-
-            echo "Usuario logado";
-
-        }
-        else{
-            echo "Falha ao logar";
-        }
-
-
-        
-    }
-
+if (isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha'])) {
+    // Acessa
+    include_once('../config.php');
     
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    // Busca o usuário no banco de dados
+    $sql_code = "SELECT * FROM tutor WHERE email = '$email' LIMIT 1";
+    $sql_exec = $conexao->query($sql_code) or die($conexao->error);
+
+    // Verifica se o usuário foi encontrado
+    if ($tutor = $sql_exec->fetch_assoc()) {
+        // Verifica a senha
+        if (password_verify($senha, $tutor['Senha'])) {
+            $_SESSION['email'] = $email;
+            header('Location: sistema.php');
+            exit();
+        } else {
+            echo "Falha ao logar: senha incorreta";
+        }
+    } else {
+        echo "Falha ao logar: usuário não encontrado";
+    }
+} else {
+    // Redireciona para a página de login se os campos estiverem vazios
+    header('Location: login.php');
+    exit();
+}
 ?>
