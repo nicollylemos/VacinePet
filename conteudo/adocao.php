04369f1a513d9ca070b54624d7f87d964579a64b
nicollylemos <nicollylemos752@gmail.com>
2024-11-11 22:26:19 -0300

    aaaaaaaaa

--- conteudo/adocao.php
+++ conteudo/adocao.php
@@ -16,77 +16,78 @@ include("../inc/header.php");
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <title>Suporte de Adoção</title>
 
+    <style>
+    .fundo {
+        background-color: #F8F8F8;
+        /* cor de fundo */
+        min-height: 100%;
+        /* Preenche toda a altura da janela */
+        display: flex;
+        /* Usando flexbox para facilitar o layout */
+        flex-direction: column;
+    }
 
+    body {
+        background-color: #419dac;
 
-    
-
+    }
+    </style>
 </head>
 
 <body>
-    <section id="full-background" class="w-100 h-100">
-        <div class="container">
-            <h1 class="title">Adote com amor,<br>
-                suporte de <span class="destaque">adoção</span><br>
-                animal.
-            </h1>
-            <p class="subtitle">Apoio e carinho na sua jornada de adoção</p>
-            <button id="btn-adocao" onclick="scrollToSection()" type="submit" class="btn btn-primary">Saiba
-                Mais</button>
-        </div>
-    </section>
-    <section id="section2">
-        <div class="parte_2">
-            <h4 class="subtitle2">Facilitando a Adoção:<br>
-                O Suporte da Nossa Veterinária</h4>
-            <div id="texto">
-                <p class="text" style="margin-bottom: 20px;">
-                    Adotar um animal é um ato de amor e responsabilidade, mas entendemos
-                    que o processo pode parecer complicado. Por isso, nossa veterinária está aqui para <span
-                        class="destaque2">facilitar</span> cada passo dessa jornada. Ao demonstrar interesse, você <span
-                        class="destaque2">não precisa se preocupar com a parte burocrática</span>. Nossa veterinária
-                    <span class="destaque2">cuidará de toda a comunicação</span> com as instituições de adoção,
-                    eliminando obstáculos e <span class="destaque2">agilizando o processo</span> para que você possa
-                    focar no que realmente importa: <span class="destaque2">dar um lar a um novo amigo</span>.
-                </p>
-
-                <p class="text" style="margin-bottom: 20px;">
-                    Com anos de experiência e dedicação, nossa veterinária garante que <span class="destaque2">cada
-                        adoção seja feita de forma consciente</span>, conectando você ao <span class="destaque2">animal
-                        que melhor se adapta ao seu estilo de vida</span>. Todo o suporte necessário é oferecido para
-                    que <span class="destaque2">essa nova etapa seja leve e especial</span>.
-                </p>
-
-                <p class="text" style="margin-bottom: 20px;">
-                    Agora, a escolha está em suas mãos. Cada animal adotado significa uma vida
-                    transformada tanto a sua quanto a dele. Entre em contato e dê o
-                    primeiro passo para trazer alegria, companheirismo e amor
-                    incondicional ao seu lar. Adotar é uma escolha que muda
-                    vidas, e a próxima pode ser a sua!
-                </p>
-
-                <a href="https://wa.me/5512991445347?text=Olá!%20Tenho%20interesse%20sobre%20a%20adoção.%20Poderia%20me%20ajudar%20com%20mais%20informações?"
-                    target="_blank" title="Fale conosco no WhatsApp"><button class="btn btn-primary btn-custom">Pronto
-                        Para Adotar?</button></a>
-
-
-
-
-
-
+    <div class="fundo">
+        <section id="full-background" class="w-100 h-100">
+            <div class="container">
+                <h1 class="title">Adote com amor,
+                    <br>suporte de <span class="destaque">adoção</span><br>animal.
+                </h1>
+                <p class="subtitle">Apoio e carinho na sua jornada de adoção</p><button id="btn-adocao"
+                    onclick="scrollToSection()" type="submit" class="btn btn-primary">Saiba Mais</button>
             </div>
+        </section>
+        <section id="section2">
+            <div class="parte_2">
+                <h4 class="subtitle2">Facilitando a Adoção:<br>O Suporte da Nossa Veterinária</h4>
+                <div id="texto">
+                    <p class="text" style="margin-bottom: 20px;">Adotar um animal é um ato de amor e responsabilidade,
+                        mas entendemos que o processo pode parecer complicado. Por isso,
+                        nossa veterinária está aqui para <span class="destaque2">facilitar</span>cada passo dessa
+                        jornada.
+                        Ao demonstrar interesse,
+                        você <span class="destaque2">não precisa se preocupar com a parte burocrática</span>. Nossa
+                        veterinária <span class="destaque2">cuidará de toda a comunicação</span>com as instituições de
+                        adoção,
+                        eliminando obstáculos e <span class="destaque2">agilizando o processo</span>para que você possa
+                        focar no que realmente importa: <span class="destaque2">dar um lar a um novo amigo</span>. </p>
+                    <p class="text" style="margin-bottom: 20px;">Com anos de experiência e dedicação,
+                        nossa veterinária garante que <span class="destaque2">cada adoção seja feita de forma
+                            consciente</span>,
+                        conectando você ao <span class="destaque2">animal que melhor se adapta ao seu estilo de
+                            vida</span>.
+                        Todo o suporte necessário é oferecido para que <span class="destaque2">essa nova etapa seja leve
+                            e
+                            especial</span>. </p>
+                    <p class="text" style="margin-bottom: 20px;">Agora,
+                        a escolha está em suas mãos. Cada animal adotado significa uma vida transformada tanto a sua
+                        quanto
+                        a dele. Entre em contato e dê o primeiro passo para trazer alegria,
+                        companheirismo e amor incondicional ao seu lar. Adotar é uma escolha que muda vidas,
+                        e a próxima pode ser a sua ! </p><a
+                        href="https://wa.me/5512991445347?text=Olá!%20Tenho%20interesse%20sobre%20a%20adoção.%20Poderia%20me%20ajudar%20com%20mais%20informações?"
+                        target="_blank" title="Fale conosco no WhatsApp"><button
+                            class="btn btn-primary btn-custom">Pronto
+                            Para Adotar?</button></a>
+                </div>
+            </div>
+        </section>
 
-        </div>
-
-    </section>
 
 
+    </div>
 </body>
 
-<!--scroll JS-->
 <script src="../js/scroll_adocao.js"></script>
-<!--swiper JS -->
-<script src="../js/swiper/swiper-bundle.min.js"></script>
-<?php include("../inc/footer.php") ?>
 
+<script src="../js/swiper/swiper-bundle.min.js"></script> <?php include("../inc/footer.php") ?>
 
 </html>
