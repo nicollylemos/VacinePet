6d3b8932dec20e0a843d303e4e0b06da4c793620
nicollylemos <nicollylemos752@gmail.com>
2024-11-19 20:55:08 -0300

    aaaaa

--- vet-pages/viewAgend.php
+++ vet-pages/viewAgend.php
@@ -59,7 +59,7 @@ if (!empty($_GET['id'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>VacinePet</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
-    <link rel="stylesheet" href="../css/css/viewAgend.css">
+    <link rel="stylesheet" href="../css/css/viewAgenda.css">
     <link rel="stylesheet" href="../css/css/responsividade/telasvetpages.css">
 </head>
 
